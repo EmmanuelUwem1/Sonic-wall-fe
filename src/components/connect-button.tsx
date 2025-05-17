a0e9b8c7a8a753a96be4e9de02b4dc57ba3daae3
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-05-17 09:27:22 +0100

    fix: add "use client" directive to layout component and simplify ConnectButton logic

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -5,16 +5,14 @@ import { WalletOptions } from "@/app/wallet-connect/wallet-options";
 import { useAccount } from "wagmi";
 export default function ConnectButton() {
    
-  function ConnectWallet() {
+  
     const { isConnected } = useAccount();
-    if (isConnected) return <Account />;
-    return <WalletOptions />;
+
+    return isConnected ? <Account /> : <WalletOptions />;
   }
- 
-    return (
-  <ConnectWallet />
-    );
-}
+
+
+
 {/* <button */}
     //     className="sm:p-3 px-3 py-2 text-base sm:text-lg rounded-lg border border-gray-600 hover:opacity-80 cursor-pointer"
     //     onClick={connectWallet}
